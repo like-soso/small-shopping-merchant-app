c99349a872379df96401d0bc59cd1961c8f2b351
GoonLee <auteur.kor@gmail.com>
2021-12-02 15:20:04 +0900

    Setup main activity

--- app/src/main/java/com/sososhopping/merchant/view/activity/MainActivity.java
+++ app/src/main/java/com/sososhopping/merchant/view/activity/MainActivity.java
@@ -1,16 +1,20 @@
 package com.sososhopping.merchant.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.sososhopping.merchant.R;
+import com.sososhopping.merchant.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
