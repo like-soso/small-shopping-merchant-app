fc9eb9b43eb72d625c830730c724a8cf7e8f834a
GoonLee <auteur.kor@gmail.com>
2021-12-02 15:27:00 +0900

    Add StartFragment

---
+++ app/src/main/java/com/sososhopping/merchant/view/fragment/StartFragment.java
@@ -0,0 +1,45 @@
+package com.sososhopping.merchant.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.sososhopping.merchant.R;
+import com.sososhopping.merchant.databinding.FragmentStartBinding;
+
+public class StartFragment extends Fragment {
+
+    private FragmentStartBinding binding;
+
+    public StartFragment() {
+
+    }
+
+    public static StartFragment newInstance() {
+        return new StartFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_start, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
